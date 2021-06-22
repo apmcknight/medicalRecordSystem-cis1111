4040ecf487a0cd87be56b8d2ecd58f9ff3f52c0c
Adam McKnight <adammcknight@me.com>
2021-06-21 22:52:04 -0400

    First Commit.

---
+++ application/main.cpp
@@ -0,0 +1,46 @@
+/* 
+(c) 2021 - Adam McKnight, All Rights Reserved.
+
+Licence Should be pasted here.
+
+Expected Code Output should be pasted here.
+
+For detailed documentation, open the index.html file in the /docs folder or see the github readme.md file in the directory root.
+*/
+
+
+#include <iostream>
+#include <string>
+//#include <math>
+
+using namespace std;
+
+int main () 
+{
+    int optionSelection;
+
+    printf("\n\n\nBON SOURS GENERAL MEDICAL CENTER\nCopyright 2021.\nFor assistance dial the IT Department x2222 form an IP phone.\n\n");
+
+    printf("To exit, press CONTROL + C -- OR -- To begin, please select an option below…\n\n");
+
+
+    printf("Option 1. Retrieve a medical record by First Name sorted alphabetically.\n");
+    
+    printf("Option 2. Retrieve a medical record by Last Name sorted alphabetically.\n");
+    
+    printf("Option 3. Recall a medical record\n");
+
+    printf("Option 4. Create a new medical record\n");
+
+    printf("Option 5. Start billing calculator\n");
+
+    cout << "Please enter your selection: ";
+    cin >> optionSelection;
+
+    //switch 
+
+    
+
+    return 0;
+
+}
