69eb754b3f1f4748bda0a22834965b08ddda5b1a
Adam McKnight <adammcknight@me.com>
2021-06-25 19:55:07 -0400

    Finished switch statement and program logic.

--- application/main.cpp
+++ application/main.cpp
@@ -16,8 +16,12 @@ For detailed documentation, open the index.html file in the /docs folder or see 
 using namespace std;
 
 int main () 
-{
-    int optionSelection;
+{    
+    int selection;
+    double billAmount;
+    double taxRate;
+   
+
 
     printf("\n\n\nBON SOURS GENERAL MEDICAL CENTER\nCopyright 2021.\nFor assistance dial the IT Department x2222 form an IP phone.\n\n");
 
@@ -32,14 +36,41 @@ int main ()
 
     printf("Option 4. Create a new medical record\n");
 
-    printf("Option 5. Start billing calculator\n");
+    printf("Option 5. Start billing calculator\n\n\n");
+
 
     cout << "Please enter your selection: ";
-    cin >> optionSelection;
+    cin >> selection;
 
-    //switch 
+    switch( selection ) {
+        case 1:
+            printf("This program is being built, and functionality for retrieving paitent records has not been released.");
+            
+            break;
+            
+        case 2:
+            printf("This program is being built, and functionality for retrieving paitent records has not been released.");
+            break;
+        
+        case 5:
+            printf("\nTo begin, please enter the amount of the bill: $");
+            cin >> billAmount;
 
-    
+            printf("\nPlease enter the tax amount as a decimal: ");
+            cin >> taxRate;
+
+            printf("\nHere's the patients total bill: $");
+            cout << (billAmount + taxRate);
+
+            printf("\n");
+
+            break;
+        
+        default:
+            printf("Please enter a valid option");
+            break;
+    } 
+
 
     return 0;
 
